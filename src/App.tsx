9112df4dc7d29a0fc2e9f45adddc2ffd6fda58a1
LAPTOP-8L5ICCUP\User <nur12345@internet.ru>
2022-09-04 23:05:41 +0300

    Component

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,35 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+    return (
+        <div>
+                <Header />
+                <Technologies />
+        </div>
+    );
+}
+
+const Technologies = () => {
+    return (
+        <div>
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JS</li>
+                <li>React</li>
+            </ul>
+        </div>
+    );
+}
+
+const Header = () => {
+    return (
+        <div>
+            <a href = '#s'>Home</a>
+            <a href= '#s'>New feed</a>
+            <a href= '#s'>Message</a>
+        </div>
+    );
 }
 
 export default App;
